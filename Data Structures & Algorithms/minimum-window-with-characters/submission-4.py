51c421ebf0758ef6c4b738a24903eddafb3375e5
Waqhar <102752628+jacodegithub@users.noreply.github.com>
2026-04-15 18:58:23 +0530

    Add: minimum-window-with-characters - submission-4

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-4.py
@@ -0,0 +1,35 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        slen = len(s)
+        tlen = len(t)
+        if tlen > slen: return ''
+        
+        min_len = float('inf')
+        res = ''
+
+        t_count = {}
+        for c in t:
+            t_count[c] = 1 + t_count.get(c, 0)
+
+        i, j = 0, 0
+        count = tlen
+        while j < slen:
+            if s[j] in t_count and t_count[s[j]] > 0:
+                count -= 1
+
+            if s[j] in t_count:
+                t_count[s[j]] -= 1
+
+            while count == 0:
+                if j-i+1 < min_len:
+                    res = s[i:j+1]
+                    min_len = j-i+1
+                if s[i] in t_count:
+                    t_count[s[i]] += 1
+                if s[i] in t_count and t_count[s[i]] > 0:
+                    count += 1
+                i += 1
+            
+            j += 1
+        
+        return res
